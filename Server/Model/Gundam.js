7fd78367fc25e210f222702629b76c70a944d9e3
Yang Chen <Yang.Chen1@us.na.ey.net>
2017-03-29 16:41:44 -0400

    correct collection name

--- Server/Model/Gundam.js
+++ Server/Model/Gundam.js
@@ -2,7 +2,7 @@
  * Created by chenya3 on 3/29/2017.
  */
 var mongoose = require('mongoose');
-mongoose.connect(process.env.ConnectionString || 'mongodb://tommychenyang:7896321psn@ds145370.mlab.com:45370/node-mongo-vue');
+mongoose.connect(process.env.ConnectionString || 'mongodb://localhost:27017/node-mongo-vue');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -13,6 +13,9 @@ db.once('open', function() {
         name: String,
         code: String,
         releaseDate: Date
+
+    },{
+        collection:'gundam'
     });
     var Gundam= mongoose.model('gundam', gundamSchema);
     module.exports =Gundam;
