b625d000bf5097b2b068823af2cd40c8f7681f29
Yang Chen <Yang.Chen1@us.na.ey.net>
2017-03-30 14:47:37 -0400

    initial set up for gulp

--- app.js
+++ app.js
@@ -4,12 +4,17 @@
 var express = require('express');
 var bodyParser= require('body-parser');
 var mongoose =require('mongoose');
+var path= require('path');
 var gundam= require('./Server/Model/Gundam');
 var router=express.Router();
 
 var app = express();
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/Client/'));
+//app.engine('html', require('ejs').renderFile);
+var root = path.dirname(require.main.filename);
+//app.set('view engine', 'html');
+//app.set('views', path.join(__dirname));
 app.set('port', (process.env.PORT || 5000));
 var gundammodel = mongoose.model('gundam');
 
@@ -19,7 +24,7 @@ router.use(function(req,res,next){
     next();
 });
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.sendFile(__dirname + '/index.html');
 });
 router.route('/gundam')
     .get(function (req, res) {
@@ -31,6 +36,15 @@ router.route('/gundam')
     });
 
 app.use('/api',router);
+var distFolder=path.resolve(__dirname, '../Client/dist');
+var srcFolder=path.resolve(__dirname);
+var staticUrl='/lib';
+app.use(express.static(srcFolder));
+app.use('/api',router);
+app.use(staticUrl, express.static(srcFolder));
+app.use(staticUrl, function(req, res, next) {
+    res.send(404); // If we get here then the request for a static file is invalid
+});
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+/**
+ * Created by chenya3 on 3/30/2017.
+ */
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+var injectOptions={
+    addPrefix:'lib',
+    addRootSlash:false
+}
+gulp.task('inject', function () {
+    var target = gulp.src('./index.html');
+    // It's not necessary to read the files (will speed up things), we're only after their paths:
+    var sources = gulp.src([
+                            'node_modules/vue/dist/vue.min.js',
+                            'node_modules/vue-router/dist/vue-router.min.js',
+                            'Client/src/**/*.js']);
+
+    return target.pipe(inject(sources,injectOptions))
+        .pipe(gulp.dest('./'));
+});
